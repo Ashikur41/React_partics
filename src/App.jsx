b852bdab536d252111ab4fd5bb75570c07909584
Goni <gonialsayed1997@gmail.com>
2025-03-19 05:37:13 +0600

    previes mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,12 @@ import Footer from './component/Footer';
 
 
 const App = () => {
-
-  // JSX Loop Inside
-
-  const city=['Dhaka','London','USA','Jessore']
-
+  
   return (
     <div>
-      <ul>
-        {
-          city.map((item,i)=>{
-
-            return <li key={i.toString}>{item}</li>
-
-          })
-        }
-      </ul>
+      <Header/>
     </div>
+
   );
 };
 
