72900bed11b2745b94480f13f7e8b333a7cc94b1
Goni <gonialsayed1997@gmail.com>
2025-03-19 02:11:01 +0600

    JSX Conventions

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,15 @@ import Footer from './component/Footer';
 const App = () => {
   return (
     <div>
-      <Header />
-      <Hero />
-      <ContactForm />
-      <Footer />
+      <p>
+        JSX CONVENTIONS<br/>
+        .You need to return a single parent element in JSX<br/>
+        .You can implement Js directly in JSX<br/>
+        .All Tags self-close in JSX<br/>
+        .className and HTML For not class and for in JSX<br/>
+        .Write all HTML Attributes in camelCase in JSX<br/>
+        .Write Inline Styles as Objects in JSX<br/>
+      </p>
     </div>
   );
 };
