7cb545a1634b9dce5496c2a9fc2f369033d4dafc
Goni <gonialsayed1997@gmail.com>
2025-03-23 15:11:10 +0600

    Conditional Rendering / using Ternary Operators

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,19 @@ import Hero from './component/Hero';
 import ContactForm from './component/ContactForm';
 import Footer from './component/Footer';
 
-const LoginStatusBtn=(status)=>{
-  if(status){
-    return <button>LogOut Btn</button>
-  }else{
-    return <button>Login Btn</button>
-  }
-}
 
 const App = () => {
+
+  let status= false;
   
-  // if and else
-
-  // const status=true;
-
-  // if(status==true){
-  //   return (
-  //     <div>
-  //       <h1>Login Status</h1>
-  //       <button>Logout</button>
-  //     </div>
-  
-  //   );
-  // }else{
-  //   return (
-  //     <div>
-  //       <h1>Login Status</h1>
-  //       <button>Login</button>
-  //     </div>
-  
-  //   );
-  // }
-
-  // fuctional
-
   return (
     <div>
-      <h1>Login Status</h1>
-      {LoginStatusBtn(false)}
+      {
+        status?
+        <button>Logout Button</button>
+        :
+        <button>Login Button</button>
+      }
     </div>
 
   );
