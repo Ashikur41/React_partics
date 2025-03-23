18b436687cd7440f0f913205c3b44bc47b997c7d
Goni <gonialsayed1997@gmail.com>
2025-03-23 15:01:29 +0600

    Conditional Rendering / using if and else

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,49 @@ import Hero from './component/Hero';
 import ContactForm from './component/ContactForm';
 import Footer from './component/Footer';
 
+const LoginStatusBtn=(status)=>{
+  if(status){
+    return <button>LogOut Btn</button>
+  }else{
+    return <button>Login Btn</button>
+  }
+}
 
 const App = () => {
   
+  // if and else
+
+  // const status=true;
+
+  // if(status==true){
+  //   return (
+  //     <div>
+  //       <h1>Login Status</h1>
+  //       <button>Logout</button>
+  //     </div>
+  
+  //   );
+  // }else{
+  //   return (
+  //     <div>
+  //       <h1>Login Status</h1>
+  //       <button>Login</button>
+  //     </div>
+  
+  //   );
+  // }
+
+  // fuctional
+
   return (
     <div>
-      <Header/>
+      <h1>Login Status</h1>
+      {LoginStatusBtn(false)}
     </div>
 
   );
+
+
 };
 
 export default App;
