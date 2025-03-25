3888b0f1db18defa56d5925a7bd38d6e89f93296
Ashikur41 <infoashikur887@gmail.com>
2025-03-25 17:36:22 +0600

    useRef() method use

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Header from './component/Header';
 import Hero from './component/Hero';
 import ContactForm from './component/ContactForm';
@@ -8,29 +8,18 @@ import Demo from './Demo';
 
 const App = () => {
   
+  let myHeadLine=useRef();
+
+  const change=()=>{
+    myHeadLine.current.innerHTML="<ul><li>A</li><li>B</li></ul>";
+    // myHeadLine.current.innerText="Hello Word";
+  }
+
   return (
     <div>
-      <h1>React Hook</h1>
-      <p>1. React Hook is a feature in the React library that allows developers
-        to use state and other React features in functional components,
-        which were previously only available in class components.
-      </p>
-      <p>2. It was introduced in React version 16.8.</p>
-      <p>3. Hooks can be used to manage state,handle side effects,and access
-        lifecycle metthods in functional components.
-      </p>
-      <p>4. There are several built-in Hooks provided by React,such as useState,
-        useEffect,useContext,and useRef.
-      </p>
-      <p>5. React Hooks have greatly simplifed the development process in React and
-        have made it easier to write reusable and composable code.
-      </p>
+      <h1 ref={myHeadLine}></h1>
 
-      <h1>React Hook</h1>
-      <h3>useRef() Method</h3>
-      <p>1. The useRef Hook allows you to persist values between renders</p>
-      <p>2. It can be used to store a mutable value that does not cause a re-render when updated.</p>
-      <p>3. It can be used to access a DOM element directly.</p>
+      <button onClick={change}>Click</button>
     </div>
 
   );
