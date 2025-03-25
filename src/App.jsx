f25f2abf343ca3e58c323a23f913a53fb4bd5026
Ashikur41 <infoashikur887@gmail.com>
2025-03-25 16:23:02 +0600

    Responding To Events

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,28 @@ import Header from './component/Header';
 import Hero from './component/Hero';
 import ContactForm from './component/ContactForm';
 import Footer from './component/Footer';
+import Demo from './Demo';
 
 
 const App = () => {
 
-  let status= true;
+ 
   
   return (
     <div>
-      <Hero title="Learn React" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."/>
+
+    <button onClick={
+      function Demo(){
+        alert('click');
+      }
+    }>Submit</button>
+
+      {/* <button onClick={()=>{
+        alert("Click Button");
+      }}>Submit</button> */}
+
+
+      {/* <button onClick={alert("click button")}>Submit</button> */}
     </div>
 
   );
